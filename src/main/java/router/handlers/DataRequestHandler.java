a1b93d818d0ef1d92fe123479380f7e1d90b5bf4
aleemsiddiqui <aleemsiddiqui029@gmail.com>
2024-04-16 02:11:55 +0530

    remove hsql connection

--- src/main/java/router/handlers/DataRequestHandler.java
+++ src/main/java/router/handlers/DataRequestHandler.java
@@ -25,27 +25,6 @@ public class DataRequestHandler extends BaseHandler{
                 System.out.println("No matching document found");
             }
         }
-//        try {
-////            Connection conn = DriverManager.getConnection("jdbc:hsqldb:mem:testdb","SA","");
-//////            String jdbcUrl = "jdbc:hsqldb:hsql://localhost:" + 9001 + "/testdb";
-//////            Connection conn = DriverManager.getConnection(jdbcUrl, "SA", "");
-////            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM table_name");
-////            ResultSet rs = stmt.executeQuery();
-////            StringBuilder responseData = new StringBuilder();
-////            while(rs.next()) {
-////                String data = rs.getString("column_name");
-////                responseData.append(data).append("\n");
-////            }
-////            rs.close();
-////            stmt.close();
-////            conn.close();
-//
-//            routingContext.response()
-//                    .putHeader("content-type", "text/plain")
-//                    .end(responseData.toString());
-//        }catch (SQLException e) {
-//            System.out.println("Error in sql statement : "+e.getMessage());
-//            routingContext.response().setStatusCode(500).end("Internal Server Error");
-//        }
+
     }
 }
