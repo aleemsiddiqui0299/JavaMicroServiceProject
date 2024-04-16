f91e6d7973a04dab0eb3e9f6d29cb112813415ac
aleemsiddiqui <aleemsiddiqui029@gmail.com>
2024-04-16 22:56:19 +0530

    refactored data req handler

--- src/main/java/router/handlers/DataRequestHandler.java
+++ src/main/java/router/handlers/DataRequestHandler.java
@@ -59,19 +59,13 @@ public class DataRequestHandler extends BaseHandler{
         }
     }
     @Override
-    protected void doHandle(RoutingContext routingContext) {
+    protected void doHandle(RoutingContext routingContext)  {
         System.out.println("GET DATA CALL");
-        String mongoConnectionUri = "mongodb+srv://<username>:<password>@<deployment-name>.mongodb.net/";
-        try(MongoClient mongoClient = MongoClients.create(mongoConnectionUri)){
-            MongoDatabase mongoDatabase = mongoClient.getDatabase("sample_mflix");
-            MongoCollection<Document> collection = mongoDatabase.getCollection("movies");
-            Document document = collection.find().first();
-            if(document != null) {
-                System.out.println("Document receieved : " + document.toJson());
-            } else {
-                System.out.println("No matching document found");
-            }
+        try{
+            Document data = getData("example_key");
         }
-
+        catch(ExecutionException e){
+            System.out.println("EXCEPTION IN MONGODB DATA CALL "+e.getMessage());
+        }
     }
 }
